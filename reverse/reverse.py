8c495c95ea7f909d1fcb1d7200f3295645037dcb
Daniel Mattox <dmattox10@gmail.com>
2020-09-11 12:03:22 -0400

    reverse done

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,9 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is not None and node.next_node is not None:
+            self.reverse_list(node.next_node, node)
+        else:
+            self.head = node
+        if prev:
+            node.set_next(prev)
